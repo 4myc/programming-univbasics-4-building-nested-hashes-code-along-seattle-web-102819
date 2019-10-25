4eca2c3060f43d0b780efd94438eda67ad56bb17
Amy C <missch3n@gmail.com>
2019-10-25 07:22:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,33 +7,33 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {
-        :nam => "Lady Montague",
-        age: "54"
+        :name => "Lady Montague",
+        :age => "54"
         #your key/value pairs here
         },
       :hero => {
-        name: "Romeo",
-        age: "15",
-        status: "alive"
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet",
-        age: "50"
+        :name => "Lord Capulet",
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
-        name: "Lady Capulet",
-        age: "51"
+        :name => "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
-        name: "Juliet",
-        age: "15",
-        status: "alive"
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
